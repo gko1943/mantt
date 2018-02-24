1f2cd4f07f83f4abedafc3aca0970f8cf480fa70
zhaofusen <zhaofusen@zhiketong.cn>
2018-02-24 10:26:20 +0800

    add pandora

---
+++ procfile.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+'use strict';
+
+module.exports = (pandora) => {
+
+  pandora
+    .fork('mantt', './bin/www');
+
+  /**
+   * you can also use cluster mode to start application
+   */
+  // pandora
+  //   .cluster('./bin/www');
+
+  /**
+   * you can create another process here
+   */
+  // pandora
+  //   .process('background')
+  //   .nodeArgs(['--expose-gc']);
+
+  /**
+   * more features please visit our document.
+   * https://github.com/midwayjs/pandora/
+   */
+
+};
